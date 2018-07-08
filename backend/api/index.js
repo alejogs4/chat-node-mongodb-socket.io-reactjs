51255a964df544a9280bfbc871dba0ad062fe4a4
alejogs4 <alejogs4@gmail.com>
2018-07-08 17:16:11 -0500

    Refactor API

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const route = express.Router()
-const { getMessages } = require('../socket/chat')
+const Message = require('../database/schema/message')
 
-route.get('/messages', getMessages)
+route.get('/messages', (req, res) => {
+  Message.find({}, (err, messages) => {
+    if(err) return res.status(500).send({ message: 'Error' })
+
+    return res.status(200).send({
+      message: 'Conseguido exitosamente',
+      messages
+    })
+  })
+})
 
 module.exports = route
--- backend/socket/chat.js
+++ backend/socket/chat.js
@@ -12,19 +12,6 @@ const sendMessages = (io, data) => {
  io.emit('print message', message)
 }
 
-const getMessages = (req, res) => {
-  Message.find({}, (err, messages) => {
-    if(err) return res.status(500).send({ message: 'Error' })
-
-    return res.status(200).send({
-      message: 'Conseguido exitosamente',
-      messages
-    })
-  })
-}
-
-
 module.exports = {
- sendMessages,
- getMessages
+ sendMessages
 }
